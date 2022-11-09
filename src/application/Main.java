92d105a1de6c767324629fb79b394d0e882f646c
Caleb Cavilla <caleb.cavilla@gmail.com>
2022-11-09 15:08:06 -0700

    First Commit. Added graphics for the main menu

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import java.io.FileInputStream;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			VBox mainMenuRoot = loader.load(new FileInputStream("src/application/MainMenuView.fxml"));
+			Scene mainMenu = new Scene(mainMenuRoot,400,400);
+			primaryStage.setScene(mainMenu);
+			primaryStage.setTitle("Sleep Tracker");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module CalebCavillaFinalProject {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
