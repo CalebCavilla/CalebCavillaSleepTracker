fd26ca900e80310c0aa8c07c1d56f658105bfc66
Caleb Cavilla <caleb.cavilla@gmail.com>
2022-11-17 21:18:33 -0700

    Auto calculate how much sleep the user wants based of goal sleep times

--- src/application/GoalSettingsController.java
+++ src/application/GoalSettingsController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class GoalSettingsController {
@@ -14,8 +15,15 @@ public class GoalSettingsController {
 	
 	String targetBedTime;
 	String targetAwakeTime;
-	double targetSleep;
+	String targetSleep;
 	
+	boolean haveGoalsBeenSet = false;
+	
+	@FXML
+	private Label calculatedSleepLabel;
+	
+    @FXML
+    private Button setGoalsButton;
 	
     @FXML
     private TextField targetBedTimeText;
@@ -40,35 +48,100 @@ public class GoalSettingsController {
     	
     	targetBedTime = targetBedTimeText.getText();
     	targetAwakeTime = targetAwakeTimeText.getText();
-    	targetSleep = Double.parseDouble(targetAwakeTimeText.getText());
     	
+    	calculateSleepTime();
     	applicationStage.setScene(settingsView);
     	applicationStage.setTitle("Settings");
     }
     
+    @FXML
     void calculateSleepTime() {
     	
-    	// Variables for the goal sleep time
-    	String bedTime = targetBedTimeText.getText();
-    	int bedTimeHours = Integer.parseInt(bedTime.substring(0, bedTime.indexOf(":")));
-    	int bedTimeMinutes = Integer.parseInt(bedTime.substring(bedTime.indexOf(":")+1));
+    	// DefaultVariables for the goal sleep time
+    	String bedTime = "12:00 am";
+    	int bedTimeHours = 0;
+    	int bedTimeMinutes = 0;
     	
-    	// Variable for the goal awake time
-    	String awakeTime = targetAwakeTimeText.getText();
-    	int awakeTimeHours = Integer.parseInt(awakeTime.substring(0, awakeTime.indexOf(":")));
-    	int awakeTimeMinutes = Integer.parseInt(awakeTime.substring(awakeTime.indexOf(":")+1));
+    	
+    	// Default Variable for the goal awake time
+    	String awakeTime = "12:00 am";
+    	int awakeTimeHours = 0;
+    	int awakeTimeMinutes = 0;
+    	
     		
 
     	
-    	while (true){
-    		if (bedTime.contains("am") && awakeTime.contains("am")){
-    			if (bedTimeMinutes > awakeTimeMinutes) {
-    				
-    			}
+    	bedTime = targetBedTimeText.getText();
+    	awakeTime = targetAwakeTimeText.getText();
+    	
+    	// update the bedtime as it is inputed
+    	if (bedTime.length() > 0) {
+       		bedTimeHours = Integer.parseInt(bedTime.substring(0, bedTime.indexOf(":")));
+          		bedTimeMinutes = Integer.parseInt(bedTime.substring(bedTime.indexOf(":")+1, bedTime.indexOf(":")+3));
+       	}
+    	
+    	// update the awake time as it is inputed
+    	if (awakeTime.length() > 0) {
+          		awakeTimeHours = Integer.parseInt(awakeTime.substring(0, awakeTime.indexOf(":")));
+          		awakeTimeMinutes = Integer.parseInt(awakeTime.substring(awakeTime.indexOf(":")+1, awakeTime.indexOf(":")+3));
+       	}
+    	
+    	// calculate the users total goal for hours of sleep
+    	
+    	// Going from the morning to the morning
+    	if (bedTime.contains("am") && awakeTime.contains("am")){
+			if (bedTimeMinutes > awakeTimeMinutes) {
+    			awakeTimeHours -= 1;
+    			awakeTimeMinutes += 60;
+    			targetSleep = (awakeTimeHours - bedTimeHours) + " hours " + (awakeTimeMinutes - bedTimeMinutes) + " minutes";
+    			calculatedSleepLabel.setText(targetSleep);
+
+    		} else if (bedTimeMinutes <= awakeTimeMinutes) {
+    			targetSleep = (awakeTimeHours - bedTimeHours) + " hours " + (awakeTimeMinutes - bedTimeMinutes) + " minutes";
+    			calculatedSleepLabel.setText(targetSleep);
     		}
+			
+		// Going from the afternoon to the morning
+    	} else if (bedTime.contains("pm") && awakeTime.contains("am")){
+    		bedTimeHours += 12;
+			if (bedTimeMinutes > 0) {
+				targetSleep = (((23 - bedTimeHours) + awakeTimeHours) + ((60 - bedTimeMinutes) + awakeTimeMinutes) / 60) + " hours " + ((60 - bedTimeMinutes) + awakeTimeMinutes) % 60 + " minutes";
+				calculatedSleepLabel.setText(targetSleep);
+
+    		} else if (bedTimeMinutes == 0) {
+    			targetSleep = (24 - bedTimeHours) + awakeTimeHours + " hours " + awakeTimeMinutes + " minutes";
+				calculatedSleepLabel.setText(targetSleep);
+    		}
+		
+		// Going from the afternoon to the afternoon
+    	} else if (bedTime.contains("pm") && awakeTime.contains("pm")){
+    		if (bedTimeMinutes > awakeTimeMinutes) {
+    			awakeTimeHours -= 1;
+    			awakeTimeMinutes += 60;
+    			targetSleep = (awakeTimeHours - bedTimeHours) + " hours " + (awakeTimeMinutes - bedTimeMinutes) + " minutes";
+    			calculatedSleepLabel.setText(targetSleep);
+
+    		} else if (bedTimeMinutes <= awakeTimeMinutes) {
+    			targetSleep = (awakeTimeHours - bedTimeHours) + " hours " + (awakeTimeMinutes - bedTimeMinutes) + " minutes";
+    			calculatedSleepLabel.setText(targetSleep);
+    		}
+    
+    	// Going from morning to the afternoon
+    	} else if (bedTime.contains("am") && awakeTime.contains("pm")){
+    		awakeTimeHours += 12;
+    		if (bedTimeMinutes > awakeTimeMinutes) {
+    			awakeTimeHours -= 1;
+    			awakeTimeMinutes += 60;
+    			targetSleep = (awakeTimeHours - bedTimeHours) + " hours " + (awakeTimeMinutes - bedTimeMinutes) + " minutes";
+    			calculatedSleepLabel.setText(targetSleep);
+
+    		} else if (bedTimeMinutes <= awakeTimeMinutes) {
+    			targetSleep = (awakeTimeHours - bedTimeHours) + " hours " + (awakeTimeMinutes - bedTimeMinutes) + " minutes";
+    			calculatedSleepLabel.setText(targetSleep);
+    		}
+    	
     	}
     }
-    
-    
+    	
 
 }
